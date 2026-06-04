288f0de8dbce6aff0c97cd65e013399f83355227
C-sewon <cswely0130@naver.com>
2026-06-04 11:42:47 +0900

    add url launcher and fix duplicate save

--- flutter_app/lib/category_screen.dart
+++ flutter_app/lib/category_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:url_launcher/url_launcher.dart'; ← 맨 위에 추가
+import 'package:url_launcher/url_launcher.dart'; 
 
 class CategoryScreen extends StatefulWidget {
   final String category;
